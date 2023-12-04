3700ac5ad1fb950784fadacc42aca1f08116c944
deanyang122 <111415664+deanyang122@users.noreply.github.com>
2023-12-04 17:30:41 +0800

    init project

---
+++ readme.md
@@ -0,0 +1,78 @@
+# Java for Practice
+
+## Overview
+
+Welcome to the "Java for Practice" project! This repository is dedicated to providing a platform for individuals seeking to hone their Java programming skills through practical exercises. Whether you're a beginner aiming to solidify your foundation or an experienced developer looking to reinforce specific concepts, this project is designed to help you enhance your Java proficiency through hands-on practice.
+
+## Table of Contents
+
+1. [Introduction](#introduction)
+2. [Getting Started](#getting-started)
+3. [Directory Structure](#directory-structure)
+4. [Contributing](#contributing)
+5. [Feedback](#feedback)
+6. [License](#license)
+
+## Introduction
+
+"Java for Practice" is a collection of Java programming exercises and examples, focusing on a range of fundamental and advanced concepts. The primary objective is to offer a structured environment for personal practice, enabling users to explore and apply Java concepts in practical scenarios.
+
+## Getting Started
+
+To embark on your journey of practicing Java concepts using this repository, follow these steps:
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/your-username/java-for-practice.git
+   ```
+
+2. **Navigate to the Project Directory:**
+   ```bash
+   cd java-for-practice
+   ```
+
+3. **Explore the Exercises:**
+   Browse through the directories to discover exercises related to specific Java concepts. Each directory contains a README file with instructions and explanations.
+
+4. **Code and Test:**
+   Write your Java code to solve the exercises. Refer to the provided README files to understand the problem statement and expected output. Test your solutions to ensure correctness.
+
+5. **Learn and Improve:**
+   Take advantage of this opportunity to delve deeper into Java concepts by researching and studying relevant documentation. Enhance your solutions based on best practices.
+
+## Directory Structure
+
+The repository is organized into directories based on different Java concepts. Each directory contains exercises related to a specific concept. Here's an overview of the directory structure:
+
+```plaintext
+java-for-practice/
+|-- concept1/
+|   |-- exercise1/
+|   |   |-- src/
+|   |   |-- README.md
+|   |-- exercise2/
+|   |   |-- src/
+|   |   |-- README.md
+|-- concept2/
+|   |-- exercise1/
+|   |   |-- src/
+|   |   |-- README.md
+|   |-- exercise2/
+|   |   |-- src/
+|   |   |-- README.md
+|-- ...
+```
+
+## Contributing
+
+If you'd like to contribute to this project by adding new exercises, improving existing ones, or suggesting enhancements, please follow the [contribution guidelines](CONTRIBUTING.md).
+
+## Feedback
+
+Your feedback is crucial! If you have suggestions, questions, or encounter any issues, please [open an issue](https://github.com/your-username/java-for-practice/issues) on this repository.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+Happy coding! 🚀
---
+++ src/thread/consumer/src/Main.java
@@ -0,0 +1,102 @@
+package thread.consumer.src;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * <a href="https://shengshengyang.github.io/2023/12/04/java-threads-consumer/">explain is here, click me</a>
+ */
+public class Main {
+    private static Buffer buffer = new Buffer();
+
+    private static class Buffer {
+        private static final int CAPACITY = 1;
+        private LinkedList<Integer> queue = new LinkedList<>();
+        private static Lock lock = new ReentrantLock();
+        private static Condition notEmpty = lock.newCondition();
+        private static Condition notFull = lock.newCondition();
+
+        public void write(int value) {
+            lock.lock();
+            try {
+                // 當緩衝區滿時，生產者等待
+                while (queue.size() == CAPACITY) {
+                    notFull.await();
+                }
+                // 寫入數據到緩衝區
+                queue.offer(value);
+                // 喚醒等待中的消費者
+                notEmpty.signalAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        public int read() {
+            lock.lock();
+            int value = 0;
+            try {
+                // 當緩衝區為空時，消費者等待
+                while (queue.isEmpty()) {
+                    notEmpty.await();
+                }
+                // 從緩衝區讀取數據
+                value = queue.remove();
+                // 喚醒等待中的生產者
+                notFull.signalAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+                return value;
+            }
+        }
+    }
+
+    private static class Consumer implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    // 消費者從緩衝區讀取數據
+                    System.out.println("Consumer reads " + buffer.read());
+                    Thread.sleep((int) (Math.random() * 1000));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static class Producer implements Runnable {
+        @Override
+        public void run() {
+            try {
+                int i = 0;
+                while (true) {
+                    // 生產者將數據寫入緩衝區
+                    System.out.println("Producer writes " + i);
+                    buffer.write(i++);
+                    Thread.sleep((int) (Math.random() * 1000));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // 使用固定大小的線程池
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        // 同時運行生產者和消費者
+        executorService.execute(new Producer());
+        executorService.execute(new Consumer());
+        // 關閉線程池
+        executorService.shutdown();
+    }
+}
